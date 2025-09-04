d89914b6da9657220c707d863972cf379db81831
Eliser Dale Bugay <eddlbugay24@bpsu.edu.ph>
2025-09-04 20:23:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/secondactivity/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.secondactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button btnClick = findViewById(R.id.btnClick);
+        final EditText etData = findViewById(R.id.etDate);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, SecondActivity.class);
+                String str;
+                str = etData.getText().toString();
+                intent.putExtra("MY_MESSAGE", str);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
